bbc588ee7bbe7d5db2c32778263159d0fbac908a
mohamedhadaj1 <46045552+mohamedhadaj1@users.noreply.github.com>
2018-12-25 16:22:11 +0000

    Update ChatServerMain.java

--- RMIChat/src/com/ensa/chat/rmi/serveur/ChatServerMain.java
+++ RMIChat/src/com/ensa/chat/rmi/serveur/ChatServerMain.java
@@ -1,14 +1,20 @@
 package com.ensa.chat.rmi.serveur;
+import java.rmi.Naming;
+
 
 import java.net.MalformedURLException;
-import java.rmi.Naming;
+
 import java.rmi.RemoteException;
 
 public class ChatServerMain {
+	
 
-	public static void main(String[] args) throws RemoteException, MalformedURLException{
+	public static void main(String[] args) throws RemoteException, MalformedURLException
+	{
+		
 		Naming.rebind("RMIChatServer", new ChatServer());
 
 	}
 
-}
+      
+ }
