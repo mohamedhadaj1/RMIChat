bd325b5091ea8729966f3af77d09d082c922547a
mohamedhadaj1 <46045552+mohamedhadaj1@users.noreply.github.com>
2018-12-25 16:21:19 +0000

    Update ChatServerIF.java

--- RMIChat/src/com/ensa/chat/rmi/serveur/ChatServerIF.java
+++ RMIChat/src/com/ensa/chat/rmi/serveur/ChatServerIF.java
@@ -1,11 +1,15 @@
 package com.ensa.chat.rmi.serveur;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
+
+import java.rmi.Remote;
 
 import com.ensa.chat.rmi.client.ChatClientIF;
 
+
 public interface ChatServerIF extends Remote{
+	
 	void registerChatClient(ChatClientIF chatClient) throws RemoteException;
-	void broadcastMessage(String message) throws RemoteException;
-}
+	
+	
+	void broadcastMessage(String message) throws RemoteException; }
